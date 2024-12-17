7f50b9d85af207579a32f9604f39d8a35d43a1ae
Paul Rivallin <99149335+Roronoatii@users.noreply.github.com>
2024-12-17 22:03:25 +0100

    Add LFSR class
    Add Lfsr class with 2 operating modes: fibonacci, galois
    galois for shifting generating
    fibonacci for binary parity generating

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import Classes.Lfsr;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String args[]) {
+        String tapsHex = "8005";
+        int loops = 10;
+        String seedHex = "1";
+        String mode = "galois";
+        Lfsr lfsr = new Lfsr((char) 0, (char) 0);
+        String result = lfsr.lfsr(tapsHex, loops, seedHex, mode);
+        System.out.println(result);
     }
 }
