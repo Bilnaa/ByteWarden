81ffd421c863a9b17403dd60ebf1d1f825c570e8
Nabil <noufnouf703@gmail.com>
2024-12-17 22:22:16 +0100

    revert : back to init state

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
-import Classes.Lfsr;
-
 public class Main {
     public static void main(String[] args) {
-        String tapsHex = "8005";
-        int loops = 10;
-        String seedHex = "1";
-        String mode = "galois";
-        Lfsr lfsr = new Lfsr((char) 0, (char) 0);
-        String result = lfsr.lfsr(tapsHex, loops, seedHex, mode);
-        System.out.println(result);
+        System.out.println("Hello, World!");
     }
 }
