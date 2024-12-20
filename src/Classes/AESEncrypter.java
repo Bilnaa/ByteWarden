69a47797820bcef7351e2235fafc10169f6fba38
snowsuit321 <g.snowsuit321@passmail.net>
2024-12-20 09:40:49 +0100

    feat : add comments and documentation to code

--- src/Classes/AESEncrypter.java
+++ src/Classes/AESEncrypter.java
@@ -8,9 +8,17 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Base64;
 
+/**
+ * Class used to encrypt and decrypt string of text using AES method.
+ * @remarks A secret key will be needed to instantiate one encrypter.
+ */
 public class AESEncrypter {
 
     // Fields
+    /**
+     * The one secret key required by the AES algorith.
+     * This secret key is based on a user inputed string.
+     */
     private SecretKeySpec secretKey;
 
     // Constructor
@@ -19,11 +27,17 @@ public class AESEncrypter {
         try
         {
             byte[] key;
+            // Convert the key string received into an array of byte.
             key = secretKey.getBytes("UTF-8");
 
-            MessageDigest hashProvider = MessageDigest.getInstance("SHA-1");
+            // Get the object required to produce a hash
+            MessageDigest hashProvider = MessageDigest.getInstance("SHA-256");
+            // store the hash of the secretKey instead of the key directly
             key = hashProvider.digest(key);
+            // Make the hashed key a fixed max 16 characters one.
             key = Arrays.copyOf(key, 16);
+            // create the encrypted secretKey for an AES based encrypter
+            // based on the hash of the string provided by user.
             this.secretKey = new SecretKeySpec(key, "AES");
         }
         catch (UnsupportedEncodingException e) {
@@ -32,25 +46,36 @@ public class AESEncrypter {
         catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
-        // this.secretKey = secretKey;
     }
 
+    /**
+     * Encrypt a plain text using the AES algorithm.
+     * @param plainText The text to encrypt.
+     * @return The encrypted text.
+     */
     // Functions
     public String encrypt(String plainText) {
         try
         {
-            //Cipher cipher = Cipher.getInstance("AES");
+            // Get the required object to encrypt plain text into the AES encryption format.
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            // Set the cipher in encryption mode
             cipher.init(Cipher.ENCRYPT_MODE, this.secretKey);
             //byte[] encrypted = cipher.doFinal(plainText.getBytes("UTF-8"));
             return Base64.getEncoder().encodeToString(cipher.doFinal(plainText.getBytes("UTF-8")));
         }
         catch (Exception e) {
+            // throw an exception if an error occur
+            // ex : the instance requested is not defined.
             throw new RuntimeException(e);
         }
 
     }
 
+    /** Decrypt a text which has been encrypted using the AES algorithm and the same secretKey.
+     * @param cipherText The text to decrypt.
+     * @return A plain text string.
+     */
     public String decrypt(String cipherText) {
         try
         {
@@ -58,7 +83,7 @@ public class AESEncrypter {
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.DECRYPT_MODE, this.secretKey);
             //byte[] encrypted = cipher.doFinal(plainText.getBytes("UTF-8"));
-//            return new String(cipher.doFinal(Base64.getDecoder().decode(cipherText)), "UTF-8");
+            // return new String(cipher.doFinal(Base64.getDecoder().decode(cipherText)), "UTF-8");
             return new String(cipher.doFinal(Base64.getDecoder().decode(cipherText)));
         }
         catch (Exception e) {
